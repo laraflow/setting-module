7f89c7a0008a3b7068e166941ba71b58ed4198b3
Hafijul Islam <hafijul233@gmail.com>
2021-11-27 14:17:00 +0600

    log viewer transferred to setting module from admin

---
+++ Routes/breadcrumbs.php
@@ -0,0 +1,15 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+Breadcrumbs::for('system.', function (BreadcrumbTrail $trail) {
+    $trail->parent('admin.');
+    $trail->push('System ', route('system.'));
+});
+
+/****************************************** System Logs ******************************************/
+Breadcrumbs::for('system.logs', function (BreadcrumbTrail $trail) {
+    $trail->parent('system.');
+    $trail->push('Logs', route('system.logs'));
+});
--- Routes/web.php
+++ Routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Rap2hpoutre\LaravelLogViewer\LogViewerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('setting')->group(function() {
+Route::prefix('setting')->name('system.')->group(function() {
     Route::get('/', 'SettingController@index');
+    //Log Viewer
+    Route::get('logs', [LogViewerController::class, 'index'])->name('logs');
 });
